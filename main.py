9b5c54e6f69556b17775d1bea7fec859241591d0
Kevin Helliwell <kbh789@gmail.com>
2022-03-15 00:25:45 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # Import the following modules
-from multiprocessing.sharedctypes import Array
 import shutil
 from datetime import date
 import os
 import sys
 
-# What it actually does
-# Makes an array of directories to copy into backup folders
+# What it actually does:
+# Creates a main backup folder
+# Makes an array of directories
 # Loops over array to copy directories into separate backup folders
-# Creates main backup folder to bundle them into
 # Moves backup folders into main backup folder
 
 # What I need this script to do:
@@ -16,30 +15,31 @@ import sys
 # 2) Copy folders/files from source folder given paths of folders/files
 # 3) Paste files to backup folder given path of backup folder
 
+# What is the path address of your source directory?
+# What are the names of the sub-directories you'd like to back up?
+# What is the path address of the directory you'd like to store your backup folder?
+# What would you like to name your backup folder?
+
+# backup_name, main_dir, sub_dir, backup_dir:
+# backup_name: "UI Backup"
+# main_dir: "C:/Program Files (x86)/World of Warcraft/_retail_/"
+# sub_dir(s): "Interface", "WTF"
+# backup_dir: "C:/Users/kbh78/Desktop"
 
 today = date.today()
 date_format = today.strftime("%d_%b_%Y_")
 
-# Makes backup folder we eventually move to
-path = "C:/Users/kbh78/Desktop"
-ui_backup_path = path + "/UI_Backup " + f"{date_format}"
-os.mkdir(ui_backup_path)
+# Makes backup folder in destination path
+main_backup_path = "C:/Users/kbh78/Desktop"
+backup_name = "UI_Backup"
+local_backup_path = main_backup_path + f"/{backup_name} " + f"{date_format}"
+os.mkdir(local_backup_path)
 
+# Makes copies of sub-directories in main backup path, moves them into backup folder
+source_path = "C:/Program Files (x86)/World of Warcraft/_retail_"
 arr = ["Interface", "WTF"]
 for i in range(0, len(arr)):
-    src = f"C:/Program Files (x86)/World of Warcraft/_retail_/{arr[i]}"
-    dst = f"C:/Users/kbh78/Desktop/{arr[i]} Backup " + f"{date_format}"
+    src = f"{source_path}/{arr[i]}"
+    dst = f"{main_backup_path}/{arr[i]} Backup " + f"{date_format}"
     shutil.copytree(src, dst)
-    shutil.move(dst, ui_backup_path)
-
-# src1 = f"C:/Program Files (x86)/World of Warcraft/_retail_/{arr[0]}"
-# dst1 = f"C:/Users/kbh78/Desktop/{arr[0]} Backup " + f"{date_format}"
-# shutil.copytree(src1, dst1)
-
-# src2 = f"C:/Program Files (x86)/World of Warcraft/_retail_/{arr[1]}"
-# dst2 = f"C:/Users/kbh78/Desktop/{arr[1]} Backup " + f"{date_format}"
-# shutil.copytree(src2, dst2)
-
-# # Moves folders to final folder
-# shutil.move(dst1, ui_backup_path)
-# shutil.move(dst2, ui_backup_path)
+    shutil.move(dst, local_backup_path)
