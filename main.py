1b47e0f8084e8b2547c3c6cd547f19325d63b07e
Kevin Helliwell <kbh789@gmail.com>
2022-03-14 23:42:03 -0400

    for loop refactoring

--- main.py
+++ main.py
@@ -17,24 +17,29 @@ import sys
 # 3) Paste files to backup folder given path of backup folder
 
 
-arr = ["Interface", "WTF"]
-
 today = date.today()
 date_format = today.strftime("%d_%b_%Y_")
-path = "C:/Users/kbh78/Desktop"
-
-src1 = f"C:/Program Files (x86)/World of Warcraft/_retail_/{arr[0]}"
-dst1 = f"C:/Users/kbh78/Desktop/{arr[0]} Backup " + f"{date_format}"
-shutil.copytree(src1, dst1)
-
-src2 = f"C:/Program Files (x86)/World of Warcraft/_retail_/{arr[1]}"
-dst2 = f"C:/Users/kbh78/Desktop/{arr[1]} Backup " + f"{date_format}"
-shutil.copytree(src2, dst2)
 
 # Makes backup folder we eventually move to
+path = "C:/Users/kbh78/Desktop"
 ui_backup_path = path + "/UI_Backup " + f"{date_format}"
 os.mkdir(ui_backup_path)
 
-# Moves folders to final folder
-shutil.move(dst1, ui_backup_path)
-shutil.move(dst2, ui_backup_path)
+arr = ["Interface", "WTF"]
+for i in range(0, len(arr)):
+    src = f"C:/Program Files (x86)/World of Warcraft/_retail_/{arr[i]}"
+    dst = f"C:/Users/kbh78/Desktop/{arr[i]} Backup " + f"{date_format}"
+    shutil.copytree(src, dst)
+    shutil.move(dst, ui_backup_path)
+
+# src1 = f"C:/Program Files (x86)/World of Warcraft/_retail_/{arr[0]}"
+# dst1 = f"C:/Users/kbh78/Desktop/{arr[0]} Backup " + f"{date_format}"
+# shutil.copytree(src1, dst1)
+
+# src2 = f"C:/Program Files (x86)/World of Warcraft/_retail_/{arr[1]}"
+# dst2 = f"C:/Users/kbh78/Desktop/{arr[1]} Backup " + f"{date_format}"
+# shutil.copytree(src2, dst2)
+
+# # Moves folders to final folder
+# shutil.move(dst1, ui_backup_path)
+# shutil.move(dst2, ui_backup_path)
