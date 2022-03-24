c91d95fd93cbac217a40a7d1b762a13ea151b3a4
Kevin Helliwell <kbh789@gmail.com>
2022-03-23 23:00:50 -0400

    comment fix

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def create_backup(source_path, source_sub_arr, backup_path, backup_name):
         shutil.move(dst, local_backup_path)
 
 
-Example
+# Example
 create_backup(
     "C:/Program Files (x86)/World of Warcraft/_retail_",
     ["Interface", "WTF"],
